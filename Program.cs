daa196604ec870c35d208007f46a71178001f8f3
Pravin Patil <Pravin.Patil@icaew.com>
2018-06-19 10:50:23 +0100

    changed to Hello repo 2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 static int Main(string[] args)
 {
   Console.WriteLine("***** My First C# App *****");
-  Console.WriteLine("Hello World, Repo1 !");
+  Console.WriteLine("Hello Repo 2!");
   Console.WriteLine();
   Console.ReadLine();
 
