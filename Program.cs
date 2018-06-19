e4ae9563941084d69bd4c46f53af5384308f9cec
Pravin Patil <Pravin.Patil@icaew.com>
2018-06-19 10:34:23 +0100

    Program.cs added

---
+++ Program.cs
@@ -0,0 +1,9 @@
+static int Main(string[] args)
+{
+  Console.WriteLine("***** My First C# App *****");
+  Console.WriteLine("Hello World!");
+  Console.WriteLine();
+  Console.ReadLine();
+
+  return 0;
+}
